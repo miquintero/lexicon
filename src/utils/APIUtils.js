c5492ace2e354e7c59f518a51de036b979b25dce
miquintero <schaeffer.quintero@gmail.com>
2019-09-16 04:55:02 +0200

    feat: created util for fetching from the API and a custom hook for comparing form values

---
+++ src/utils/APIUtils.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+// import handleErrors from './handleErrors';
+
+const url = process.env.API_URL;
+const key = process.env.API_KEY;
+
+const extraFields = process.env.ADDITIONAL_FIELDS;
+const polarization = process.env.POLARIZATION;
+
+const APIRequests = {
+
+  getHeaders() {
+    return {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+  },
+  
+  allLanguages () {
+    return axios.get(
+      `${url}/languages?apiKey=${key}`,
+      this.getHeaders()
+    )
+    .then(response => {
+      const languages = response.data;
+      return languages;
+    })
+    .catch(error => console.log(error))
+  },
+
+  semanticallySimilarWords (word) {
+    return axios.get(
+      `${url}/lexicon/es/${word}?additionalFields=${extraFields}&apiKey=${key}&polarizeWord=${polarization}`,
+      this.getHeaders()
+    )
+    .then(response => {
+      const fetchedWords = response.data.semanticallySimilarWords
+        .map(wordDetails => wordDetails.word);
+      return fetchedWords;
+    })
+    .catch(error => console.log(error))
+  },
+
+  wordInformation () {
+    return axios.get(
+      `${url}/lexicon/es/ganas/info?apiKey=${key}`,
+      this.getHeaders()
+    )
+    .then(response => {
+      const details = [response.data.frequency, response.data.absoluteRank];
+      return details;
+    })
+    .catch(error => console.log(error))
+  }
+}
+
+export default APIRequests;
